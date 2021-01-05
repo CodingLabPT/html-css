156fb4fa3906c600667249babd351b0eb8f8a390
M@rio <mariomendesdeveloper@gmail.com>
2021-01-05 16:33:30 +0000

    Add files via upload

---
+++ SimpleColorPiker/js/script.js
@@ -0,0 +1,6 @@
+function color() {
+    let colorPiker = document.getElementById('colorPiker');
+    let ColorText = document.getElementById('ColorText');
+    //document.body.style.backgroundColor = colorPiker.value;
+    ColorText.value = colorPiker.value;
+}
